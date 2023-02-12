030b435ca384066e375bd4bdcf37a6b207302a6d
Srivatsan-code <srivatsangsm2000@gmail.com>
2023-02-12 14:51:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
-    ""
+    <div>
+    <nav className='nav'>
+    <img className='logo' src={logo} alt="logo" />
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Menu</li>
+      <li>Reservations</li>
+      <li>Order Online</li>
+      <li>Login</li>
+    </ul>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+      
+    </footer>
+    </div>
   );
 }
 
